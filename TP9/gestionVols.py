40891b13abdb6acf12d2c0a3d1c5189e66353b9c
Léo SAMUEL <leosamuel64@gmail.com>
2022-07-20 11:56:30 +0200

    TP render_template

--- TP9/gestionVols.py
+++ TP9/gestionVols.py
@@ -10,27 +10,34 @@ def index():
     vols=[[id,dep,arr,hdep,harr] for (id,dep,arr,hdep,harr) in 
                 (ligne.split(' ') for ligne in open('data/flights.txt'))]
     
-    return render_template('index.html',vols=vols)
+    return render_template('header_home.html')+render_template('index.html',vols=vols)+render_template('footer.html')
 
 @app.route ('/ajouter.html')
 def ajouter() :
-    return render_template('ajouter.html')
+    vols=[[id,dep,arr,hdep,harr] for (id,dep,arr,hdep,harr) in 
+                (ligne.split(' ') for ligne in open('data/flights.txt'))]
+    
+    return render_template('header.html',page_name="GSEA - Ajouter")+render_template('ajouter.html',vols=vols)+render_template('footer.html')
 
 @app.route ('/connecter.html')
 def connecter() :
-    return render_template('connecter.html')
+    return render_template('header.html',page_name="GSEA - Connexion")+render_template('connecter.html')+render_template('footer.html')
 
 @app.route ('/contact.html')
 def contact() :
-    return render_template('contact.html')
+    return render_template('header.html',page_name="GSEA - Contact")+render_template('contact.html')+render_template('footer.html')
 
 @app.route ('/modifier.html')
 def modifier() :
-    return render_template('modifier.html')
+    vols=[[id,dep,arr,hdep,harr] for (id,dep,arr,hdep,harr) in 
+                (ligne.split(' ') for ligne in open('data/flights.txt'))]
+    return render_template('header.html',page_name="GSEA - Modifier")+render_template('modifier.html',vols=vols)+render_template('footer.html')
 
 @app.route ('/supprimer.html')
 def supprimer() :
-    return render_template('supprimer.html')
+    vols=[[id,dep,arr,hdep,harr] for (id,dep,arr,hdep,harr) in 
+                (ligne.split(' ') for ligne in open('data/flights.txt'))]
+    return render_template('header.html',page_name="GSEA - Supprimer")+render_template('supprimer.html',vols=vols)+render_template('footer.html')
 
 
 if __name__ == '__main__' :
